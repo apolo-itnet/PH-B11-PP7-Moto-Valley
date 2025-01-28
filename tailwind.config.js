d7e561d69bc1b7a6ae380e2b880b615897010500
Apolo Barua Apurbo <apolo.itnet@gmail.com>
2025-01-28 22:23:44 +0600

    PH-B11-PP7-Moto-Valley-Hero-Section-Carousel-Update

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./index.html",         
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'bike-bg': "url('/images/slider/bg/bike-bg-1.webp')",
+      }
+    }
+  },
+  plugins: [],
+}
